345f891653246950bb5e7bae32fcad09895d98ce
FungiMatt <mattds.2608@gmail.com>
2019-04-03 21:57:16 -0300

    Alterações adiconado uso do módulo properties-reader, para suporte a arquivos de propriedades; adicionado arquivo conf.properties; adicionada verificação do hdd onde a aplicação se encontra (apenas windows); alterada forma como o json para post é gerado; adiciondas chamadas ao logger para printar erros na criação do request.

--- usgReader.js
+++ usgReader.js
@@ -4,6 +4,10 @@ const os = require('os');
 const osUtils = require('os-utils');
 const si = require('systeminformation');
 var mkdirp = require('mkdirp');
+var http = require('http');
+
+var PropertiesReader = require('properties-reader');
+var properties = PropertiesReader('conf.properties');
 
 require('console-stamp')(console, '[HH:MM:ss]');
 
@@ -58,14 +62,10 @@ function tudo(){
 		logger.info(`Memória em uso = ${memUsada}GB`);
 		logger.info(`Memória Livre = ${memLivre}GB`);
 
-		let dadosMem = {
-      memoria_total: memTotal,
-      memoria_uso: memUsada,
-      memoria_livre: memLivre,
-    };
+		let jsonMem = `"memoria_total": ${memTotal},\n`
+		+`"memoria_usada": ${memUsada},\n`
+		+`"memoria_livre": ${memLivre},`;
 
-    let jsonMem = JSON.stringify(dadosMem, null, 2);
-		fs.writeFileSync('mem.json', jsonMem);
 		jsonPost = jsonMem;
 	});
 
@@ -73,6 +73,7 @@ function tudo(){
     var i;
 
     for (i=0;i<disco.length;i++){
+			var raiz = process.cwd();
       const hdd = disco[i].fs;
       const espacoTotal = ((disco[i].size/bToGb).toFixed(0));
       const espacoUso = ((disco[i].used/bToGb).toFixed(0));
@@ -80,41 +81,39 @@ function tudo(){
       const espacoDisp = (((disco[i].size/bToGb).toFixed(0))-((disco[i].used/bToGb).toFixed(0)));
       const percDisp = (100-percUso);
 
-      console.log(`Disco ${hdd}`);
-			logger.info(`Disco ${hdd}`);
+			if(`${hdd}\\ws\\install\\usgReader`==raiz||hdd=='C:'){
+	      console.log(`Disco ${hdd}`);
+				logger.info(`Disco ${hdd}`);
 
-      if(isNaN(espacoTotal)){
-        console.log('Informações de espaço indisponíveis');
-				logger.info('Informações de espaço indisponíveis');
-      } else{
-        console.log(`Espaço total = ${espacoTotal}GB`);
-				logger.info(`Espaço total = ${espacoTotal}GB`);
-        console.log(`Espaço em uso = ${espacoUso}GB (${percUso}%)`);
-				logger.info(`Espaço em uso = ${espacoUso}GB$ (${percUso}%)`);
-        console.log(`Espaço disponível = ${espacoDisp}GB (${percDisp}%)`);
-				logger.info(`Espaço disponível = ${espacoDisp}GB$ (${percDisp}%)`);
-      }
+	      if(isNaN(espacoTotal)){
+	        console.log('Informações de espaço indisponíveis');
+					logger.info('Informações de espaço indisponíveis');
+	      } else{
+	        console.log(`Espaço total = ${espacoTotal}GB`);
+					logger.info(`Espaço total = ${espacoTotal}GB`);
+	        console.log(`Espaço em uso = ${espacoUso}GB (${percUso}%)`);
+					logger.info(`Espaço em uso = ${espacoUso}GB$ (${percUso}%)`);
+	        console.log(`Espaço disponível = ${espacoDisp}GB (${percDisp}%)`);
+					logger.info(`Espaço disponível = ${espacoDisp}GB$ (${percDisp}%)`);
+	      }
 
-			var jsonDisco;
-			var dadosDisco;
+				var jsonDisco;
+				var dadosDisco;
 
-			dadosDisco = {
-        disco_unidade: hdd,
-        disco_espaco_total: espacoTotal,
-        disco_espaco_usado: espacoUso,
-        disco_espaco_disponivel: espacoDisp,
-      };
+				dadosDisco = `{\n"disco_unidade": "${hdd}",\n`
+	        +`"disco_espaco_total": ${espacoTotal},\n`
+	        +`"disco_espaco_usado": ${espacoUso},\n`
+	        +`"disco_espaco_disponivel": ${espacoDisp}\n}`;
 
-			if (i==0){
-				jsonDisco = JSON.stringify(dadosDisco, null, 2);
-			} else {
-				jsonDisco = `${jsonDisco},\n${JSON.stringify(dadosDisco, null, 2)}`;
+				if (i==0||!jsonDisco){
+					jsonDisco = dadosDisco;
+				} else {
+					jsonDisco = `${jsonDisco},\n${dadosDisco}`;
+				}
 			}
     }
 
-		fs.writeFileSync('hdd.json', jsonDisco);
-		jsonPost = `${jsonPost},\n${jsonDisco}`;
-
+		jsonPost = `${jsonPost}\n"discos": [\n${jsonDisco}\n],`;
   });
 
 	osUtils.cpuUsage(function(cpuPerc){
@@ -130,50 +129,58 @@ function tudo(){
 		console.log(`Uso de CPU (%): ${usoCPU}`);
 		logger.info(`Uso de CPU (%): ${usoCPU}`);
 
-		let dadosCPU = {
-			cpu_perc_uso_atual: usoCPU,
-		};
+		let jsonCPU = `"cpu_usado": ${usoCPU}`
 
-		let jsonCPU = JSON.stringify(dadosCPU, null, 2);
-		fs.writeFileSync('cpu.json', jsonCPU);
-		jsonPost = `${jsonPost},\n${jsonCPU}`;
+		jsonPost = `${jsonPost}\n${jsonCPU}`;
 	});
 
 	console.log("=".repeat(70));
 	logger.info("=".repeat(70));
 
 	var configDest = {
-		hostname: 'localhost',
-		port: 6660,
+		hostname: properties.get('host_api'),
+		//port: 3000,
+		path: '/monitorings',
 		method: 'POST',
 		headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				'Content-Length': dataMem.length
+				'Content-Type': 'application/json'
 		}
 	};
 
-	var req = http.request(options, function (res) {
-			console.log('STATUS:', res.statusCode);
-			console.log('HEADERS:', JSON.stringify(res.headers));
+	var dataJson = new Date(Date.now()).toLocaleString();
+	var dataJsonFormat = format(dataJson, 'isoDateTime')
 
-			res.setEncoding('utf8');
+	if(jsonPost){
+		jsonPost = `{\n"empresa": "${properties.get('empresa')}",\n${jsonPost},\n"data_registro": "${dataJsonFormat}"\n}`
 
-			res.on('data', function (chunk) {
-					console.log('BODY:', chunk);
-			});
+		var req = http.request(configDest, function (res) {
+				console.log('STATUS:', res.statusCode);
+				logger.info('STATUS:', res.statusCode);
+				console.log('HEADERS:', JSON.stringify(res.headers));
+				logger.info('HEADERS:', JSON.stringify(res.headers));
 
-			res.on('end', function () {
-					console.log('No more data in response.');
-			});
-	});
+				res.setEncoding('utf8');
 
-	req.on('error', function (e) {
-			console.log('Problem with request:', e.message);
-	});
+				res.on('data', function (chunk) {
+						console.log('BODY:', chunk);
+						logger.info('BODY:', chunk);
+				});
 
-	req.write(jsonPost);
-	req.end();
+				res.on('end', function () {
+						console.log('No more data in response.');
+				});
+		});
 
+		req.on('error', function (e) {
+				console.log('Problem with request:', e.message);
+				logger.error('Problem with request:', e.message);
+				logger.error(jsonPost);
+		});
+
+		req.write(jsonPost);
+		logger.info(jsonPost);
+		req.end();
+	}
 };
 
 if(intervalo==0){
@@ -184,5 +191,5 @@ if(intervalo==0){
 }
 
 if (intervalo>0){
-	setInterval(tudo, 5000);
+	setInterval(tudo, properties.get('interval'));
 };
